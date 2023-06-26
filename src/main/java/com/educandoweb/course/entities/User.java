21e702f5331eb83bad41ce739533b56ca3aa4c10
Ryan Lanziloti <pitagoraslanz@gmail.com>
2023-06-26 10:57:54 -0300

    Entity order

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +18,8 @@ public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	
+	
 	@Id
 	@GeneratedValue(strategy  = GenerationType.IDENTITY)
 	private Long id;
@@ -22,6 +27,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -81,6 +89,10 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
